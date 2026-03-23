93da969182b00d3bf928b403ec4acaeff5e4418f
OmarHost Deploy <deploy@omarhost.local>
2026-03-23 09:22:27 -0400

    Initial OMAR HOST deployment

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.omarhost.app',
+  appName: 'OmarHostMobile',
+  webDir: 'dist/public'
+};
+
+export default config;
